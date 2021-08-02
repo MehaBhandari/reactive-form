479967b8dad71c79586ef107e2c6bc11e425bf3d
Meha <ahem4progs@gmail.com>
2021-08-02 16:42:22 +0530

    created a basic skeleton of angular app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ReactiveFormComponent} from './reactive-form/reactive-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'reactiveFormComponent', component: ReactiveFormComponent, pathMatch: 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,

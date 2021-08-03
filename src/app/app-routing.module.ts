58824a737819c322d08e15885ae3099f5ee399b9
Meha <ahem4progs@gmail.com>
2021-08-03 17:04:32 +0530

    Initial form html done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ReactiveFormComponent} from './reactive-form/reactive-form.component';
 
+
 const routes: Routes = [
   {path: 'reactiveFormComponent', component: ReactiveFormComponent, pathMatch: 'full'}];
 
--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,10 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 
 @Component({
     selector: 'app-reactive-form',
-    templateUrl: './reactive-form.component.html'
+    templateUrl: './reactive-form.component.html',
+    styleUrls: ['../app.component.scss']
 })
 
-export class ReactiveFormComponent {
+export class ReactiveFormComponent implements OnInit {
     public formHeading = 'This is the Reactive Form: ';
+    EmployeeForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit() {
+        this.EmployeeForm = this.fb.group({
+            firstName: [],
+            lastName: [],
+            emailId: []
+        });
+    }
+
+    public onButtonSubmit() {
+        alert('Submitted');
+    }
 }

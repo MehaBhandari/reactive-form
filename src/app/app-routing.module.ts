4cbe71afa419adbdfa5a64e8c6c9262c19e1a552
Meha <ahem4progs@gmail.com>
2022-07-27 15:04:24 +0530

    Registered CRUD in module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from './app.component';
 import {ReactiveFormComponent} from './reactive-form/reactive-form.component';
 import {LoginFormComponent} from './login-form/login-form.component';
+import {CrudOperations} from './crud-operations/crud-operations.component';
 
 const routes: Routes = [
   {path: 'reactiveFormComponent', component: ReactiveFormComponent, pathMatch: 'full'},
-  {path: 'loginFormComponent', component: LoginFormComponent, pathMatch: 'full'}
+  {path: 'loginFormComponent', component: LoginFormComponent, pathMatch: 'full'},
+  {path: 'crudOperations', component: CrudOperations, pathMatch: 'full'}
 ]
 
 @NgModule({

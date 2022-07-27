aee3cf73283d2674e4d73e356e31c422d294bb11
Meha <ahem4progs@gmail.com>
2022-07-27 15:50:26 +0530

    Created layout for product detail and listing

---
+++ src/app/crud-operations/product-detail/product-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-product-detaiil',
+    templateUrl: './product-detail.component.html',
+    styleUrls: ['./product-detail.component.html']
+})
+
+export class ProductDetail {}

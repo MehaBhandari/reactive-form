3c2803d6eb7c11086a3e3fb2877d66e248b7ed7e
Meha <ahem4progs@gmail.com>
2022-07-27 15:03:08 +0530

    Added CRUD layout

---
+++ src/app/crud-operations/crud-operations.component.ts
@@ -0,0 +1,9 @@
+import {} from ""
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-crud",
+    templateUrl: "./crud-operations.component.html"
+})
+
+export class CrudOperations {}
